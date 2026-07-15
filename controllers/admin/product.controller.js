fd6448de831c6492917864673f005e0285ba5f8d
phuongne <doanthanhphuong29012006@gmail.com>
2026-07-15 13:43:45 +0700

    Log product creation

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require('../../models/product.model');
 const ProductCategory = require('../../models/product-category.model');
+const Account = require('../../models/account.model');
 const systemConfig = require('../../config/system');
 
 const filterStatusHelper = require('../../helpers/filterStatus');
@@ -48,6 +49,16 @@ module.exports.index = async (req, res) => {
     .sort(sort)
     .limit(objectPagination.limitItems)
     .skip(objectPagination.skip);
+
+    for (const product of products) {
+        const user = await Account.findOne({
+            _id: product.createdBy.account_id
+        });
+
+        if (user) {
+            product.userFullName = user.fullName;
+        }
+    }
 
     res.render('admin/pages/products/index', {
         pageTitle: "Danh sách sản phẩm",
@@ -160,6 +171,10 @@ module.exports.createPost = async (req, res) => {
         req.body.position = countProduct + 1;
     } else {
         req.body.position = parseInt(req.body.position);
+    }
+
+    req.body.createdBy = {
+        account_id: res.locals.user.id
     }
 
     const product = new Product(req.body);
