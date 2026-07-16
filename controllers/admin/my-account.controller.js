3fc934ce5082cb9b1eeb9dace72e96e4ed7aec3f
phuongne <doanthanhphuong29012006@gmail.com>
2026-07-16 22:09:14 +0700

    Account information update page

---
+++ controllers/admin/my-account.controller.js
@@ -0,0 +1,51 @@
+const md5 = require('md5');
+const Account = require('../../models/account.model');
+const systemConfig = require('../../config/system');
+
+// [GET] /admin/my-account
+module.exports.index = (req, res) => {
+    res.render('admin/pages/my-account/index', {
+        pageTitle: "Thông tin cá nhân"
+    });
+};
+
+// [GET] /admin/my-account/edit
+module.exports.edit = async (req, res) => {
+    res.render('admin/pages/my-account/edit', {
+        pageTitle: "Chỉnh sửa thông tin cá nhân"
+    });
+};
+
+// [PATCH]/admin/my-account/edit/
+module.exports.editPatch = async (req, res) => {
+    const id = res.locals.user.id;
+
+    const emailExist = await Account.findOne({
+        _id: { $ne: id },
+        email: req.body.email,
+        deleted: false
+    });
+
+    if (emailExist) {
+        req.flash('error', "Email này đã tồn tại!");
+
+        const currentUrl = req.get('Referrer');
+        res.redirect(currentUrl);
+    } else {
+        if (req.body.password) {
+            req.body.password = md5(req.body.password);
+        } else {
+            delete req.body.password;
+        }
+        
+        try {
+            await Account.updateOne({ _id: id }, req.body);
+            req.flash('success', "Cập nhật tài khoản thành công!");
+        } catch (error) {
+            req.flash('error', "Cập nhật tài khoản thất bại!");
+        }
+
+        const currentUrl = req.get('Referrer');
+        res.redirect(currentUrl);
+    }
+}
