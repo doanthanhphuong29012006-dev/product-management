ba5b2211c87ea3f1ca26a2ae054853eb38c5af3d
phuongne <doanthanhphuong29012006@gmail.com>
2026-07-16 22:08:38 +0700

    Account information page

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -8,6 +8,7 @@ const productCategoryRoutes = require('./product-category.route');
 const roleRoutes = require('./role.route');
 const accountRoutes = require('./account.route');
 const authRoutes = require('./auth.route');
+const myAccountRoutes = require('./my-account.route');
 
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
@@ -41,11 +42,17 @@ module.exports = (app) => {
         accountRoutes
     );
 
+    app.use(
+        PATH_ADMIN + '/my-account', 
+        authMiddleware.requireAuth, 
+        myAccountRoutes
+    );
+
     app.use(PATH_ADMIN + '/auth', authRoutes);
 
-//     app.use('*', (req, res) => {
-//         res.status(404).render('admin/pages/errors/404', {
-//             pageTitle: "404 Không tìm thấy trang"
-//         });
-//     });
+    // app.use('*', (req, res) => {
+    //     res.status(404).render('admin/pages/errors/404', {
+    //         pageTitle: "404 Không tìm thấy trang"
+    //     });
+    // });
 }
