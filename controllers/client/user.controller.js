269107cf44eea15406312c2f24f3623bd884a0f8
phuongne <doanthanhphuong29012006@gmail.com>
2026-07-20 14:19:59 +0700

    Login feature

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -18,13 +18,57 @@ module.exports.registerPost = async (req, res) => {
         req.flash('error', "Email đã tồn tại!");
         const currentUrl = req.get('Referrer');
         res.redirect(currentUrl);
-        return
+        return;
     }
 
     req.body.password = md5(req.body.password);
 
     const user = new User(req.body);
     user.save();
+
+    res.cookie("tokenUser", user.tokenUser);
+
+    res.redirect("/");
+}
+
+// [GET]/user/login
+module.exports.login = (req, res) => {
+    res.render('client/pages/user/login',{
+        pageTitle: "Đăng nhập tài khoản"
+    });
+}
+
+// [POST]/user/login
+module.exports.loginPost = async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await User.findOne({
+        email: email,
+        deleted: false
+    });
+
+    if (!user) {
+        req.flash('error', "Tài khoản không tồn tại!");
+        const currentUrl = req.get('Referrer');
+        res.redirect(currentUrl);
+        return;
+    }
+
+    if (md5(password) !== user.password) {
+        console.log(password);
+        req.flash('error', "Mật khẩu không chính xác!");
+        const currentUrl = req.get('Referrer');
+        res.redirect(currentUrl);
+        return;
+    }
+
+    if (user.status === "inactive") {
+        req.flash('error', "Tài khoản đang bị khóa!");
+        const currentUrl = req.get('Referrer');
+        res.redirect(currentUrl);
+        return;
+    }
 
     res.cookie("tokenUser", user.tokenUser);
 
--- validates/client/user.validate.js
+++ validates/client/user.validate.js
@@ -25,3 +25,23 @@ module.exports.registerPost = (req, res, next) => {
     
     next();
 }
+
+module.exports.loginPost = (req, res, next) => {
+    if (!req.body.email) {
+        req.flash('error', "Vui lòng nhập email!");
+
+        const currentUrl = req.get("Referrer");
+        res.redirect(currentUrl);
+        return;
+    }
+
+    if (!req.body.password) {
+        req.flash('error', "Vui lòng nhập mật khẩu!");
+
+        const currentUrl = req.get("Referrer");
+        res.redirect(currentUrl);
+        return;
+    }
+    
+    next();
+}
