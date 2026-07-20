8203c592e9fa73f3dee9056f29781868b3fe39fe
phuongne <doanthanhphuong29012006@gmail.com>
2026-07-20 09:35:39 +0700

    Register an account

---
+++ validates/client/user.validate.js
@@ -0,0 +1,27 @@
+module.exports.registerPost = (req, res, next) => {
+    if (!req.body.fullName) {
+        req.flash('error', "Vui lòng nhập tên!");
+
+        const currentUrl = req.get("Referrer");
+        res.redirect(currentUrl);
+        return;
+    }
+
+    if (!req.body.email) {
+        req.flash('error', "Vui lòng nhập email!");
+
+        const currentUrl = req.get("Referrer");
+        res.redirect(currentUrl);
+        return;
+    }
+
+    if (!req.body.password) {
+        req.flash('error', "Vui lòng nhập mật khẩu!");
+
+        const currentUrl = req.get("Referrer");
+        res.redirect(currentUrl);
+        return;
+    }
+    
+    next();
+}
